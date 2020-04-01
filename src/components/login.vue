3e17496e882fe52ef95ee2a1d3f5089ebbfa9b52
咦我的头发呢 <18715838915@163.com>
2020-04-01 22:07:20 +0800

    添加表单验证

--- src/components/login.vue
+++ src/components/login.vue
@@ -8,19 +8,19 @@
 
         
         <!-- 登录表单区域 -->
-        <el-form  label-width="0px" class="login-form">
+        <el-form :model="user"  ref="loginRef" :rules="userRules" label-width="0px" class="login-form">
             <!-- 用户名 -->
-            <el-form-item>
-                 <el-input prefix-icon="el-icon-s-custom" ></el-input>
+            <el-form-item prop="username">
+                 <el-input v-model="user.username" prefix-icon="el-icon-s-custom" ></el-input>
                 </el-form-item>
             <!-- 密码 -->
-            <el-form-item >
-                 <el-input   prefix-icon="el-icon-lock"></el-input>
+            <el-form-item prop="password" >
+                 <el-input  v-model="user.password"  type="password" prefix-icon="el-icon-lock"></el-input>
                 </el-form-item>
                 <!-- 按钮 -->
             <el-form-item class="btn">
-                 <el-button type="primary">登录</el-button>
-                 <el-button type="info">重置</el-button>
+                 <el-button type="primary" @click="login">登录</el-button>
+                 <el-button type="info" @click="resetForm">重置</el-button>
                 </el-form-item>
         </el-form>
 
@@ -29,7 +29,53 @@
     </div>
 </template>
 <script>
-export default {}
+export default {
+
+    data(){
+        return{
+            //绑定的表单对象
+            user:{
+                username:"",
+                password:""
+            },
+            //验证规则
+            userRules:{
+                username:[
+                    { required: true, message: '请输入用户名', trigger: 'blur' },
+                     { min: 4, max: 11, message: '长度在 4 到 11 个字符', trigger: 'blur' }
+                ],
+                password:[
+                    { required: true, message: '请输入用户密码', trigger: 'blur' },
+                     { min: 4, max: 11, message: '长度在 4 到 11 个字符', trigger: 'blur' }
+                ]
+            }
+        }
+    },
+    methods:{
+        //点击重置按钮，重置登录表单
+        resetForm(){
+            //获取表单对象，执行重置方法
+            this.$refs.loginRef.resetFields();
+        },
+        //登录方法
+        login(){
+            //调用表单校验回调函数
+            this.$refs.loginRef.validate((valid) =>{
+                
+                if(!valid){
+                    return;
+                }
+            //表单的验证是根据之前添加的验证规则进行判断的,如果满足验证规则
+            //valid为true  否则为false
+
+
+            })
+              
+
+        }
+    }
+
+}
 </script>
 <style lang="less" scoped>
 .login-contanier{
--- src/main.js
+++ src/main.js
@@ -5,7 +5,13 @@ import './plugins/element.js'
 
 // 导入全局样式表
 import './assets/css/global.css'
+//导入axios
+import axios from 'axios'
+//添加axios到Vue
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
