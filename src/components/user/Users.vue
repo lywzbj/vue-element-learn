90d9872718112b68e0f35f1c321ba77dd5f29df2
咦我的头发呢 <18715838915@163.com>
2020-04-07 22:09:13 +0800

    完成用户数据渲染，分页

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -19,6 +19,54 @@
         </el-col>
 
     </el-row>
+
+    <el-table :data="users" :stripe="true" max-height="700px">
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="用户名" prop="username"></el-table-column>
+        <el-table-column label="密码" prop="password"></el-table-column>
+        <el-table-column label="时间" prop="lastvisitdate"></el-table-column>
+        <el-table-column label="角色" prop="role"></el-table-column>
+        <el-table-column label="标志" >
+          <template slot-scope="scope">  
+        <el-switch v-model="scope.row.flag">
+                          </el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template >
+            <!-- 需要按钮 -->
+            <el-tooltip  effect="light" content="编辑用户" placement="top" :enterable="false">
+              <el-button type="primary" icon="el-icon-edit"></el-button>
+            </el-tooltip>
+              <!-- 分配权限按钮 -->
+              <el-tooltip  effect="light" content="分配权限" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" ></el-button>
+              </el-tooltip>
+              <!-- 删除按钮 -->
+              <el-tooltip  effect="light" content="删除用户" placement="top" :enterable="false">
+              <el-button type="danger" icon="el-icon-delete"></el-button>
+              </el-tooltip>
+          </template>
+        </el-table-column>
+
+    </el-table>
+
+    <!-- 分页 -->
+<template>
+  <div class="block">
+    <!-- <span class="demonstration">完整功能</span> -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[10, 20, 30, 40]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
+  </div>
+</template>
+
     </el-card>
 
 
@@ -29,7 +77,40 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+    return {
+          pageNum:1,
+          pageSize:30,
+          users:[],
+          currentPage:1,
+          total:0,
+    }
+  },
+  created(){
+    this.getUsers();
+  },
+  methods:{
+   async getUsers(){
+      const {data:res} = await this.$axios.get('http://localhost:8088/user/list/'+this.pageNum+'/'+this.pageSize);
+      // if(res.meta.status !== 200){
+      //   return this.message.error("数据获取失败!");
+      // }
+      this.users = res.data;
+      this.total=res.pager.total;
+      this.totalPage = this.total/this.pageSize;
+    },
+    handleSizeChange(newPageSize){
+        this.pageSize = newPageSize;
+        this.pageNum = 1;
+        this.getUsers();
+    },
+    handleCurrentChange(currentPage){
+      this.currentPage = currentPage;
+      this.getUsers();
+    }
+  }
+};
 </script>
 
 <style  lang="less" scoped>
@@ -44,6 +125,9 @@ export default {};
   .box-card {
     
   }
-
+  .el-table{
+    margin-top:15px;
+    font-size: 16px;
+  }
 
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -16,7 +16,12 @@ import {
       BreadcrumbItem,
       Card,
       Row,
-      Col
+      Col,
+      Table,
+      TableColumn,
+      Switch,
+      Tooltip,
+      Pagination
     } from 'element-ui'
 
 Vue.use(Button)
@@ -35,5 +40,10 @@ Vue.use(BreadcrumbItem)
 Vue.use(Card)
 Vue.use(Row)
 Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
 
 Vue.prototype.$message = Message
